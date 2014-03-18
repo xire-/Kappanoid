cb1aca7af5d0d56dfd696275f63e53e013f392ff
Marco Gasparini <thepvo@gmail.com>
2014-03-18 23:37:04 +0100

    added a sort of namespace for kappanoid

--- src/js/kappanoid.js
+++ src/js/kappanoid.js
@@ -2,10 +2,31 @@
  * Kappanoid game
  * Authors: Francesco Cagnin, Marco Gasparini
  */
-function helloKappa() {
-    alert('hello kappa!');
-}
 
-$(document).ready(function() {
-    helloKappa();
+//trick to create a namespace
+var kappanoid = (function () {
+    "use strict";
+    //private stuff
+    function initCanvas(canvas) {
+        console.log(canvas);
+        canvas.width = 800;
+        canvas.height = 600;
+    }
+
+    //public stuff
+    return {
+        version: '0.0',
+        init: function () {
+            initCanvas($('#gameCanvas')[0]);
+
+            var color = $('#optColor');
+            console.log(color);
+        }
+    };
+}());
+
+// function to execute once the document is ready
+$(document).ready(function () {
+    "use strict";
+    kappanoid.init();
 });
