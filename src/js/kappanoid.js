db48218591a6c03bef16bc8def26b6c66f0ab496
integeruser <integer@live.it>
2014-03-19 10:59:44 +0000

    added settings example

--- src/js/kappanoid.js
+++ src/js/kappanoid.js
@@ -3,25 +3,41 @@
  * Authors: Francesco Cagnin, Marco Gasparini
  */
 
+/*jslint browser: true, devel: true */
+/*global $*/
+
 // trick to create a namespace
 var kappanoid = (function () {
     "use strict";
     // private stuff
-    function initCanvas(canvas) {
+    var settings, init, initCanvas;
+
+
+    settings = {
+        canvasWidth: 800,
+        canvasHeight: 600,
+        canvasBackgroundColor: '#babbe0'
+    };
+
+
+    init = function () {
+        initCanvas($('#gameCanvas')[0]);
+
+        var color = $('#optColor');
+        console.log(color);
+    };
+
+    initCanvas = function (canvas) {
         console.log(canvas);
-        canvas.width = 800;
-        canvas.height = 600;
-    }
+        canvas.width = settings.canvasWidth;
+        canvas.height = settings.canvasHeight;
+    };
+
 
     // public stuff
     return {
         version: '0.0',
-        init: function () {
-            initCanvas($('#gameCanvas')[0]);
-
-            var color = $('#optColor');
-            console.log(color);
-        }
+        init: init
     };
 }());
 
