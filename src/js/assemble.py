1b9c7ce2bcf2fac85a9e65be4e477ffd44aeede6
Marco Gasparini <thepvo@gmail.com>
2014-03-20 21:21:51 +0100

    updated assemble script to strip assertion if not in debug

--- src/js/assemble.py
+++ src/js/assemble.py
@@ -5,33 +5,44 @@ import re
 from datetime import datetime
 
 def main():
-    #legge il path di parts da arg
-    if len(sys.argv) < 2:
-        print 'usage: {} js_path'.format(sys.argv[0])
+    # read options from argv
+    if len(sys.argv) < 3:
+        print (
+            'usage: {0} js_path output_file [-d | --debug]\n'
+            'example: {0} src/js/ assembled.js -d\n'
+            'optional debug flag leave assertions in the assembled file.'
+            ).format(sys.argv[0])
         exit(1)
-    js_path = sys.argv[1]
-    output_file = sys.argv[2] if len(sys.argv) > 2 else 'assembled.js'
 
-    # legge /js/parts/main.js
+    js_path = sys.argv[1]
+    output_file = sys.argv[2]
+    debug_flag = (sys.argv[3] == '-d' or sys.argv[3] == '--debug') if len(sys.argv) > 3 else False
+
+    # read /js/parts/main.js
     with open(js_path + 'parts/main.js') as fmain, open(js_path + output_file, 'w') as fris:
+        # print timestamp on top
         fris.write('/* Generated: {} */\n\n'. format(datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
-        # fris.write(fmain.read())
         for line in fmain:
             m = re.match(r'^(\s*)// __import__ (.*)$', line)
             if m is not None:
-                # se trova direttive di import
+                # if it's an import directive
                 indent = m.group(1)
                 import_path = m.group(2)
-                # legge file indicato e sostituisce il contenuto alla riga di import
+                # put the content of the indicated file in the final file
                 with open(js_path + 'parts/' + import_path, 'r') as fimp:
                     for l in fimp:
                         if re.match(r'^\s*$', l):
                             fris.write('\n')
                         else:
                             fris.write(indent + l)
-            else:
-                # non e' uno statement di import
-                fris.write(line)
+                continue
+
+            m = re.match(r'^(\s*)console\.log\(.*\);$', line)
+            if m is not None and not debug_flag:
+                continue
+
+            # it's not a special statement, don't modify
+            fris.write(line)
 
 
 if __name__ == '__main__':
