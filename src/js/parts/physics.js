bf93488a3a5e3cdb87b820325d690f6450eef4e3
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-03-21 20:11:57 +0100

    fixed error in function "normalize"; added an assertion in function "normalize"; added the test function "testVector2"

--- src/js/parts/physics.js
+++ src/js/parts/physics.js
@@ -63,9 +63,11 @@ var baseVector2 = Object.defineProperties({}, {
 
     normalize: {
         value: function() {
-            var scale = this.len();
+            var scale = this.length();
             this.x /= scale;
             this.y /= scale;
+
+            console.assert(this.length() === 1);
             return this;
         }
     },
@@ -112,3 +114,21 @@ var Vector2 = function(x, y) {
     }
 };
 Vector2.prototype = baseVector2;
+
+function testVector2() {
+    var vec1 = new Vector2();
+    console.assert(vec1.x === 0 && vec1.y === 0);
+
+    vec1 = new Vector2(3);
+    console.assert(vec1.x === 0 && vec1.y === 0);
+
+    vec1 = new Vector2(3, 4);
+    console.assert(vec1.x === 3 && vec1.y === 4);
+    console.assert(vec1.length() === 5);
+    console.assert(vec1.squaredLength() === 25);
+    console.assert(vec1.clone().normalize().length() === 1);
+
+    var vec2 = new Vector2(3, 4);
+    console.assert(vec1.distance(vec2) === 0);
+    console.assert(vec1.squaredDistance(vec2) === 0);
+}
