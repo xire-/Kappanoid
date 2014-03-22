203b54c0ede5ae2b1f88e3ef111798997ed81fe0
Marco Gasparini <thepvo@gmail.com>
2014-03-22 13:32:41 +0100

    implemented main loop; added FPScount variable to namespace

--- src/js/parts/main.js
+++ src/js/parts/main.js
@@ -10,29 +10,77 @@
 var kappanoid = (function() {
     'use strict';
     // private stuff
-    var settings, init, initCanvas;
 
+    // keep count of the current mean frame per second
+    var currentFPS = 0;
 
-    settings = {
+    // store all the configurable settings
+    var settings = {
         canvasWidth: 800,
         canvasHeight: 600,
         canvasBackgroundColor: '#babbe0'
     };
 
 
-    init = function() {
-        initCanvas($('#gameCanvas')[0]);
+    var init = function() {
+        // TODO bind keys to actions
+        // TODO generate settings interface
+        initCanvas();
 
-        var color = $('#optColor');
-        console.log(color);
+        startMainLoop();
     };
 
-    initCanvas = function(canvas) {
-        console.log(canvas);
+    var initCanvas = function() {
+        var canvas = $('#gameCanvas')[0];
+
         canvas.width = settings.canvasWidth;
         canvas.height = settings.canvasHeight;
     };
 
+    var startMainLoop = function() {
+        // timestamp of last game loop iteration (used to calculate delta time)
+        var lastTime = Date.now();
+        // frames since last FPS update
+        var count = 0;
+        // sum of all FPS since last update
+        var totalFps = 0;
+
+        setInterval(
+            function() {
+                var currentTime = Date.now();
+                var elapsed = currentTime - lastTime;
+                lastTime = currentTime;
+
+                // calculate fps
+                totalFps += 1000 / elapsed;
+                count += 1;
+                if (count % 50 === 0) {
+                    currentFPS = totalFps / 50;
+                    totalFps = 0;
+                    count = 0;
+                }
+
+                var steps = 10;
+                var ministep = elapsed / steps;
+
+                while (steps > 0) {
+                    updateGame(ministep);
+                    steps -= 1;
+                }
+
+                renderGame(elapsed);
+            },
+            16
+        );
+    };
+
+    var renderGame = function(delta) {
+        //TODO render
+    };
+
+    var updateGame = function(delta) {
+        // TODO update physics
+    };
 
     /////////////////////////////////// Physics
     // __import__ physics.js
