5aa34f63de498d25a113f7458ce1418920176c72
Marco Gasparini <thepvo@gmail.com>
2014-03-24 23:30:12 +0100

    fixed comments

--- src/js/parts/physics.js
+++ src/js/parts/physics.js
@@ -1,9 +1,8 @@
 ///////////////// Vector2
 // __import__ physics-Vector2.js
 
-///////////////// Vector2
+///////////////// Easing
 // __import__ physics-Easing.js
-
 
 ///////////////// CollisionDetection
 // __import__ physics-CollisionDetection.js
