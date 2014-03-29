0f4ea5cddf70013b8e24a9b9d653d95b92b9110d
Marco Gasparini <thepvo@gmail.com>
2014-03-29 20:16:36 +0100

    fixed random function again lol

--- src/js/parts/main.js
+++ src/js/parts/main.js
@@ -187,7 +187,7 @@ var kappanoid = (function() {
             max = min;
             min = 0;
         }
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.floor(Math.random() * (max - min)) + min;
     };
 
 
