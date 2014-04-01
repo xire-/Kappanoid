3792104e884de60ecf385c8e10b779d15435d97f
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-04-01 10:14:21 +0200

    ops

---
+++ src/js/parts/levels.js
@@ -0,0 +1,13 @@
+var levels = [{
+    backgroundColor: '#000000',
+    bricks: [
+        new Brick(new Vector2(800 / 2, 400 / 2), new Vector2(25, 10), 1, constants.brickColor),
+        // new Brick(new Vector2(800/2 - 200, 400/2), new Vector2(25,10), 3, constants.brickColor),
+    ],
+}, {
+    backgroundColor: '#000000',
+    bricks: [
+        new Brick(new Vector2(800 / 2, 400 / 2), new Vector2(25, 10), 1, constants.brickColor),
+        // new Brick(new Vector2(800/2 - 200, 400/2), new Vector2(25,10), 3, constants.brickColor),
+    ],
+}];
