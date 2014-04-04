ab7dc6d490c9d900d3f610d9f44897437f192a09
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-04-04 13:36:56 +0200

    reworked shadeColor method

--- src/js/parts/utils.js
+++ src/js/parts/utils.js
@@ -60,20 +60,17 @@ var getColorString = function(color) {
  * lighten or darken a hex color by a specific percentage
  */
 var shadeColor = function(color, percent) {
-    var R = parseInt(color.substring(1, 3), 16);
-    var G = parseInt(color.substring(3, 5), 16);
-    var B = parseInt(color.substring(5, 7), 16);
+    var R = parseInt(color.r * (100 + percent) / 100);
+    var G = parseInt(color.g * (100 + percent) / 100);
+    var B = parseInt(color.b * (100 + percent) / 100);
 
-    R = parseInt(R * (100 + percent) / 100);
-    G = parseInt(G * (100 + percent) / 100);
-    B = parseInt(B * (100 + percent) / 100);
+    R = R + Math.floor((percent / 255) * R);
+    G = G + Math.floor((percent / 255) * G);
+    B = B + Math.floor((percent / 255) * B);
 
-    R = (R < 255) ? R : 255;
-    G = (G < 255) ? G : 255;
-    B = (B < 255) ? B : 255;
-
-    var RR = ((R.toString(16).length == 1) ? '0' + R.toString(16) : R.toString(16));
-    var GG = ((G.toString(16).length == 1) ? '0' + G.toString(16) : G.toString(16));
-    var BB = ((B.toString(16).length == 1) ? '0' + B.toString(16) : B.toString(16));
-    return '#' + RR + GG + BB;
+    return {
+        r: R,
+        g: G,
+        b: B,
+    };
 };
