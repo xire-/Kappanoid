4b19739d8f20998f684bffa17f06d470eee8787f
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-04-04 19:56:28 +0200

    refactoring

--- src/js/parts/gameStates-GameOver.js
+++ src/js/parts/gameStates-GameOver.js
@@ -1,15 +1,6 @@
 var GameOverState = function() {
-    var keyPress = function(e) {
-        switch (e.keyCode) {
-            case 32: // SPACE
-                //world.releaseBalls();
-                // prevent space from scrolling the page
-                return false;
-            default:
-                // alert(e.keyCode);
-                break;
-        }
-    };
+
+    ///////// public methods
 
     var renderDestroyEverything = function() {
         g.save();
@@ -30,8 +21,22 @@ var GameOverState = function() {
         ///world.update(delta);
     };
 
+    var keyPress = function(e) {
+        switch (e.keyCode) {
+            case 32: // SPACE
+                //world.releaseBalls();
+                // prevent space from scrolling the page
+                return false;
+            default:
+                // alert(e.keyCode);
+                break;
+        }
+    };
+
+    ///////// constructor
 
     var constructor = function GameOverState() {
+        // public methods
         this.render = renderDestroyEverything;
         this.update = updateDestroyEverything;
         this.keyPress = keyPress;
