eda7d5b98a5285fc063d2f2422f9eab01af366a5
Francesco Cagnin <francesco.cagnin@gmail.com>
2014-04-04 15:10:11 +0200

    added rgbToHsl and isColor functions; removed shadeColor function

--- src/js/parts/utils.js
+++ src/js/parts/utils.js
@@ -1,3 +1,7 @@
+var isColor = function(value) {
+    return (value.r !== undefined && value.g !== undefined && value.b !== undefined) || (value.h !== undefined && value.s !== undefined && value.l !== undefined);
+};
+
 /*
  * clamp val to the interval [min, max]
  */
@@ -30,6 +34,44 @@ var randomInt = function(min, max) {
 };
 
 /*
+ * convert a rgb color to an hsl color
+ */
+function rgbToHsl(color) {
+    var r = color.r / 255;
+    var g = color.g / 255;
+    var b = color.b / 255;
+
+    var max = Math.max(r, g, b);
+    var min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+                break;
+        }
+        h /= 6;
+    }
+
+    return {
+        h: Math.floor(h * 360),
+        s: Math.floor(s * 100),
+        l: Math.floor(l * 100),
+    };
+}
+
+/*
  * given an object with rgb or hsl properties, returns the corresponding string
  * an optional alpha value can be added to the object, with name 'a'
  */
@@ -39,7 +81,7 @@ var getColorString = function(color) {
     var a = (color.a !== undefined) ? clamp(0, color.a, 1).toFixed(3) : 1;
 
     if (color.r !== undefined) {
-        //it's rgb or rgba
+        // it's rgb or rgba
         var r = clamp(0, Math.floor(color.r), 255);
         var g = clamp(0, Math.floor(color.g), 255);
         var b = clamp(0, Math.floor(color.b), 255);
@@ -47,30 +89,11 @@ var getColorString = function(color) {
         return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
     }
     if (color.h !== undefined) {
-        //it's hsl or hsla
+        // it's hsl or hsla
         var h = color.h % 360;
         var s = clamp(0, color.s, 100);
         var l = clamp(0, color.l, 100);
 
         return 'hsla(' + h + ', ' + s + '%, ' + l + '%, ' + a + ')';
     }
-};
-
-/*
- * lighten or darken a hex color by a specific percentage
- */
-var shadeColor = function(color, percent) {
-    var R = parseInt(color.r * (100 + percent) / 100);
-    var G = parseInt(color.g * (100 + percent) / 100);
-    var B = parseInt(color.b * (100 + percent) / 100);
-
-    R = R + Math.floor((percent / 255) * R);
-    G = G + Math.floor((percent / 255) * G);
-    B = B + Math.floor((percent / 255) * B);
-
-    return {
-        r: R,
-        g: G,
-        b: B,
-    };
 };
