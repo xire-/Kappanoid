7b597e6a54f95ca1bdbac1dcd865a697b73a4791
Francesco Cagnin <francesco.cagnin@gmail.com>
2018-06-06 13:55:31 +0200

    Cleanup

--- assemble.py
+++ assemble.py
@@ -5,50 +5,48 @@ import sys
 from datetime import datetime
 
 # global set of all imported files
-importedFiles = set()
+imported_files = set()
 
 
 # remove all assertions and test functions from the output file
 # return the processed text
 def remove_debug_code(text):
     # remove test functions
-    ris = re.sub(re.compile(r'\n*( *)function test\S*\(\) \{\n.*?\n\1\}', re.DOTALL), '', text)
+    res = re.sub(re.compile(r'\n*( *)function test\S*\(\) \{\n.*?\n\1\}', re.DOTALL), '', text)
+    # remove asserts
+    res = re.sub(r'\n *console\.assert\(.*\);', '', res)
+    return res
 
-    # remove reamining asserts
-    ris = re.sub(r'\n *console\.assert\(.*\);', '', ris)
-    return ris
 
-
-# find import statement and replace them with the content of the indicated file
-# if a file has already been imported it will be ignored
+# find import statements and replace them with the content of the imported file (ignoring files already imported)
 # return the processed text
-def process_import(text, js_path, debug_flag):
-    # search for import statement and replace content
+def process_import(text, js_path, debug):
     def import_callback(m):
-        global importedFiles
+        global imported_files
         indentation = m.group(1)
         file_name = m.group(2)
 
-        if file_name in importedFiles:
-            print('{} is already imported'.format(file_name))
+        if file_name in imported_files:
+            print('{} already imported'.format(file_name))
             return ''
-        importedFiles.add(file_name)
+        imported_files.add(file_name)
 
         with open(js_path + 'parts/' + file_name, 'r') as f:
             file_content = f.read()
 
-        ris = process_import(file_content, js_path, debug_flag)
+        res = process_import(file_content, js_path, debug)
 
         # add correct indentation
-        ris = re.sub(r'(.*?\n)', indentation + r'\1', ris)
-        # if last line doesn't have final \n
-        ris = re.sub(r'\n(.*?)$', r'\n' + indentation + r'\1', ris)
+        res = re.sub(r'(.*?\n)', indentation + r'\1', res)
+        # if last line doesn't have final '\n'
+        res = re.sub(r'\n(.*?)$', r'\n' + indentation + r'\1', res)
+        res = '\n' + res
+        return res
 
-        return '\n' + ris
-
-    if not debug_flag:
+    if not debug:
         text = remove_debug_code(text)
 
+    # search for import statement and replace content
     return re.sub(r'\n( *)// __import__ (.*)', import_callback, text)
 
 
@@ -57,42 +55,41 @@ def strip_empty_line_spaces(text):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Assemble parts of javascript into a single file.')
+    parser = argparse.ArgumentParser()
 
     parser.add_argument(
         '-d',
         '--debug',
-        action="store_true",
+        action='store_true',
         default=False,
         help='leave debug functions and asserts in the output file',
     )
 
     parser.add_argument(
         'js_path',
-        action="store",
-        help='path to the js folder, must contain a parts sub folder',
+        action='store',
+        help='path to the "js" folder, which must contain a "parts" folder',
     )
 
     parser.add_argument(
         '-o',
         '--outputFile',
-        action="store",
-        dest="outputFile",
+        action='store',
+        dest='outputFile',
         type=argparse.FileType('w'),
-        default=sys.stdout,
-        help='output file. Default: stdout',
+        help='output file',
     )
     args = parser.parse_args()
 
     js_path = args.js_path
-    fris = args.outputFile
-    debug_flag = args.debug
+    fres = args.outputFile
+    debug = args.debug
 
     with open(js_path + 'parts/main.js', 'r') as fmain:
         # print timestamp on top
-        fris.write('/* Generated: {} */\n\n'.format(datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
-        ris = process_import(fmain.read(), js_path, debug_flag)
-        fris.write(strip_empty_line_spaces(ris))
+        fres.write('/* Generated: {} */\n\n'.format(datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
+        res = process_import(fmain.read(), js_path, debug)
+        fres.write(strip_empty_line_spaces(res))
 
 
 if __name__ == '__main__':
